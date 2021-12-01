b1532bb742ce7d8459901d35d11065e36ffca839
alysoncampos <alyson.siqueiracampos@gmail.com>
2021-12-01 19:34:10 -0300

    @alysoncampos[implementation components entrar-cadastrar]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     MenuComponent,
-    RodapeComponent
+    RodapeComponent,
+    EntrarComponent,
+    CadastrarComponent
   ],
   imports: [
     BrowserModule,
