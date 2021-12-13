3efbedb87f842b2325ceab0d332a7e8a654dff64
alysoncampos <alyson.siqueiracampos@gmail.com>
2021-12-13 15:03:57 -0300

    @alysoncampos[Implementação do post e get de postagens]

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
@@ -14,12 +14,24 @@ export class AuthService {
     private http: HttpClient
   ) { }
 
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
+  refreshToken(){
+    this.token = {headers: new HttpHeaders().set('Authorization', environment.token)}
+  }
+
   entrar(userLogin: UserLogin): Observable<UserLogin>{
     return this.http.post<UserLogin>('https://alysonblogpessoal.herokuapp.com/usuarios/logar', userLogin)
   }
 
   cadastrar(usuario: Usuario):Observable<Usuario>{
     return this.http.post<Usuario>('https://alysonblogpessoal.herokuapp.com/usuarios/cadastrar', usuario)
+  }
+
+  getByIdUsuario(id: number): Observable<Usuario>{
+    return this.http.get<Usuario>(`https://alysonblogpessoal.herokuapp.com/usuarios/${id}`, this.token)
   }
 
   logado(){
